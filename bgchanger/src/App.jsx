eecca032f9c58e61ee84b712312168393a41a91f
bhavishy09 <futuresingh111@gmail.com>
2025-01-18 23:47:10 +0530

    add new projects

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("white");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div
+      className=" h-screen duration-200 w-full"
+      style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap  justify-center inset-x-0 bottom-12 px-4 ">
+        
+        <div className="fixed flex flex-wrap  justify-center gap-4 shadow-xl bg-zinc-600 px-3 py-1 rounded-3xl">
+
+          <button  onClick={() => setColor("black")} className="outline-neutral-50 px-3 py-1 rounded-full text-white shadow-lg">black</button>
+         
+
+          <button onClick={() => setColor("red")} className="outline-neutral-50 px-3 py-1 rounded-full text-white shadow-lg">red</button>
+         
+
+          <button onClick={() => setColor("blue")}  className="outline-neutral-50 px-3 py-1 rounded-full text-white shadow-lg">blue</button>
+          <button  onClick={() => setColor("green")}  className="outline-neutral-50 px-3 py-1 rounded-full text-white shadow-lg">green</button>
+
+          </div>
+          </div>
+
+          </div>
+
+        
+       
+
+       
+  
   )
 }
 
--- talwindprops/src/App.jsx
+++ talwindprops/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from  './coponents/Card'
-import Music from  './coponents/Music'
+
 
 function App() {
   const [count, setCount] = useState(0)
