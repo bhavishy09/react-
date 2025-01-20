15af3b0d4909369ad8d38418b3891c7bf15662eb
bhavishy09 <futuresingh111@gmail.com>
2025-01-21 00:41:49 +0530

    add new project cureency convertor

---
+++ curencyconvertor/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <>
+   <h1 className='text-3xl rounded-non bg-orange-600 text-white'>paisa badlo (curencyconvertor)</h1>
+   </>
+  )
+}
+
+export default App
---
+++ curencyconvertor/src/hooks/usepaisainfo.js
@@ -0,0 +1,10 @@
+import { useEffect,useState } from "react";
+
+ function usepaisainfo(currency)
+ {
+    useEffect(()=>{},[])
+
+
+
+
+ }
